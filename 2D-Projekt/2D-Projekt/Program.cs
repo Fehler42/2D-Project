0055365ee6a04415abd700f21bb99ffe4555c6a4
JOCP9733 <jan-ole.perschewski@st.ovgu.de>
2014-11-04 20:20:24 +0100

    Creat window reimplementet SFML

--- 2D-Projekt/2D-Projekt/Program.cs
+++ 2D-Projekt/2D-Projekt/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+
 
 namespace _2D_Projekt
 {
@@ -10,7 +13,50 @@ namespace _2D_Projekt
     {
         static void Main(string[] args)
         {
-            //test,importiert
+            // Erzeuge ein neues Fenster
+            RenderWindow win = new RenderWindow(new VideoMode(800, 600), "Mein erstes Fenster");
+
+            // Achte darauf, ob Fenster geschlossen wird
+            win.Closed += win_Closed;
+
+            initialize();
+            loadContent();
+
+            // Das eigentliche Spiel
+            while (win.IsOpen())
+            {
+                // Schauen ob Fenster geschlossen werden soll
+                win.DispatchEvents();
+
+                update();
+                draw(win);
+            }
+        }
+        static void win_Closed(object sender, EventArgs e)
+        {
+            // Fenster Schließen
+            ((RenderWindow)sender).Close();
+        }
+        static void loadContent()
+        {
+
+        }
+
+        static void update()
+        {
+
+        }
+
+        static void draw(RenderWindow win)
+        {
+            win.Clear(new Color());
+
+            win.Display();
+        }
+        static void initialize()
+        {
+
+
         }
     }
 }
